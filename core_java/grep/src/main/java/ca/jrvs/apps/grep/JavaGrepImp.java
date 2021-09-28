d84f0d6a3a9b550eb00cc20158d2ec419fbf28a2
Minh Phuong Ly <phuong.ly.k2set@gmail.com>
2021-09-28 02:43:09 +0000

    complete java grep app implemenation ticket

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -4,9 +4,13 @@ import com.sun.org.slf4j.internal.Logger;
 import com.sun.org.slf4j.internal.LoggerFactory;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 
 public class JavaGrepImp implements JavaGrep{
@@ -35,33 +39,75 @@ public class JavaGrepImp implements JavaGrep{
 
     @Override
     public void process() throws IOException {
-        ArrayList<String> matchedLines;
-
-//        for file in listFilesRecursively(rootDir)
-//        for line in readLines(file)
-//        if containsPattern(line)
-//        matchedLines.add(line)
-//        writeToFile(matchedLines)
+        List<String> matchedLines = new ArrayList<>();
+        List<File> files = listFiles(getRootPath());
+        for(File file: files){
+            List<String> fileContent = readFLines(file);
+            for (String line: fileContent){
+                if (containsPattern(line)){
+                    matchedLines.add(line);
+                }
+            }
+        }
+        writeToFile(matchedLines);
     }
 
     @Override
     public List<File> listFiles(String rootDir) {
-        return null;
+        List<File> result = new ArrayList<>();
+        File directoryPath = new File(rootDir);
+        File[] filesList = directoryPath.listFiles();
+        if (filesList !=null){
+            for(File file:filesList){
+                if(file.isFile()){
+                    result.add(file);
+                }
+                else if(file.isDirectory()){ //make recursive call if it's a directory
+                    result.addAll(listFiles(file.getAbsolutePath()));
+                }
+            }
+        }
+        return result;
     }
 
     @Override
     public List<String> readFLines(File inputFile) {
-        return null;
+        List<String> result = new ArrayList<>();
+        try{
+            File myFile = new File(inputFile.getAbsolutePath());
+            Scanner reader = new Scanner(myFile);
+            while(reader.hasNextLine()){
+                result.add(reader.nextLine());
+            }
+            reader.close();
+        }
+        catch (FileNotFoundException e){
+            logger.error("File not found!",e);
+            throw new RuntimeException("Error: File not found!",e);
+        }
+        catch (Exception e){
+            logger.error("Error happened",e);
+            throw new RuntimeException("Error happened!!",e);
+        }
+        return result;
     }
 
     @Override
     public boolean containsPattern(String line) {
-        return false;
+        return Pattern.matches(getRegex(),line);
     }
 
     @Override
     public void writeToFile(List<String> lines) throws IOException {
-
+        try (PrintWriter myWriter = new PrintWriter(getOutFile())){
+            for(String line: lines){
+                myWriter.println(line);
+            }
+        }
+        catch ( FileNotFoundException e){
+            logger.error("File not found!",e);
+            throw  new FileNotFoundException("File not found!");
+        }
     }
 
     @Override
